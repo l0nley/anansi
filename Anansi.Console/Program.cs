5f4bc3181481a80c272500c5caf5d5d8a1602690
Uladzimir Harabtsou <Uladzimir Harabtsou>
2016-09-20 05:49:55 -0700

    adding some

--- Anansi.Console/Program.cs
+++ Anansi.Console/Program.cs
@@ -14,7 +14,9 @@
 				Console.WriteLine("LCD Clear");
 				display.Clear(0x00);
 				Console.WriteLine("LCD Reactangle");
-				display.Rectangle(10, 10, 50, 50, 0xFF);
+				display.Rectangle(10, 10, 30, 40, 0xFF);
+				Console.WriteLine("LCD Rectangle Empty");
+				display.EmptyRectangle(10, 50, 30, 70, 0xFF, 0xAD);
 				//display.LoadFont("font.bmp", 12, 16, 96);
 
 			}
